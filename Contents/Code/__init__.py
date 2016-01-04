be14ddf61c3640fd943d35e57558c302d27d3a4d
Solvek <sa@mobiwolf.com>
2016-01-04 18:36:18 +0200

    Pluging development

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,26 +1,46 @@
 TITLE = 'VsetvNet'
 WEB_PAGE = 'http://vsetv.net'
+ART    = 'art-default.jpg'
+ICON   = 'icon-default.png'
 
 
 ####################################################################################################
 def Start():
-    ObjectContainer.title1 = TITLE
+
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	ObjectContainer.title1 = TITLE
+	ObjectContainer.art = R(ART)
+	DirectoryObject.art = R(ART)
+	DirectoryObject.thumb = R(ICON)
 
 ####################################################################################################
 @handler('/video/vsetvnet', TITLE)
 def MainMenu():
-  oc = ObjectContainer()
 
-  for video in XML.ElementFromURL(WEB_PAGE).xpath('//html/body/div[@id="con"]/div[@class="con_tv"]/div[@class="left"]/div[@class="section"]/div[@class="box visible"]/div[@id="scrol"]/a')
-  	url = WEB_PAGE + video.xpath('./@href')[0].text
-  	span = video.xpath('./div/span')
-  	title = span.xpath('./noindex/div[@class="kanal_title"]')[0].text
-  	summary = span.xpath('./div[@class="kanal_trans"]')[0].text
+	oc = ObjectContainer()
 
-  	oc.add(VideoClipObject(
-      url = url,
-      title = title,
-      summary = summary
-    ))
+	for video in HTML.ElementFromURL(WEB_PAGE).xpath('//html/body/div[@id="con"]/div[@class="con_tv"]/div[@class="left"]/div[@class="section"]/div[@class="box visible"]/div[@id="scrol"]/a'):
 
-  return oc
+		url = WEB_PAGE + video.xpath('./@href')[0]		
+
+		path = './div/span/'
+		titlePath = 'div[contains(@class, "kanal_title")]'
+
+		title = video.xpath(path+titlePath)
+
+		if len(title) < 1:
+			title = video.xpath(path+'noindex/'+titlePath)
+
+		title = title[0].text
+
+		summary = video.xpath(path+'div[contains(@class,"kanal_trans")]')[0].text
+
+		Log.Debug("Clip url: %s, title: %s, summary: %s" % (url, title, summary, ))
+
+		oc.add(VideoClipObject(
+			url = url,
+			title = title,
+			summary = summary
+		))
+
+	return oc
