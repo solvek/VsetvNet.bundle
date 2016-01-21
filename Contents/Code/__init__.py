a658b0fc88be1ee9dc0f7c3f6cace6b2abf548a7
Solvek <sa@mobiwolf.com>
2016-01-21 17:13:11 +0200

    Fixed icon for STB channel

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,11 +3,12 @@ WEB_PAGE = 'http://vsetv.net'
 ART    = 'art-default.jpg'
 ICON   = 'icon-default.png'
 
+SUBSTITUTIONS = {'stbua' : '7'}
 
 ####################################################################################################
 def Start():
 
-	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	Plugin.AddViewGroup("List", viewMode="InfoList", mediaType="items")
 	ObjectContainer.title1 = TITLE
 	ObjectContainer.art = R(ART)
 	DirectoryObject.art = R(ART)
@@ -17,13 +18,18 @@ def Start():
 @handler('/video/vsetvnet', TITLE)
 def MainMenu():
 
-	oc = ObjectContainer()
+	oc = ObjectContainer(view_group="List")
 
 	for video in HTML.ElementFromURL(WEB_PAGE).xpath('//html/body/div[@id="con"]/div[@class="con_tv"]/div[@class="left"]/div[@class="section"]/div[@class="box visible"]/div[@id="scrol"]/a'):
 
 		subpath = video.xpath('./@href')[0]
 		url = WEB_PAGE + subpath
-		thumb = WEB_PAGE + '/templates/tv/images'+subpath[:-4]+'png'
+		file_name = subpath[1:-5]
+
+		if file_name in SUBSTITUTIONS:
+			file_name = SUBSTITUTIONS[file_name]
+
+		thumb = WEB_PAGE + '/templates/tv/images/'+file_name+'.png'
 
 		path = './div/span/'
 		titlePath = '/div[contains(@class, "kanal_title")]'
